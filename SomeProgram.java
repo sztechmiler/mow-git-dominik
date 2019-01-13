b2262e526e3503f28cb7ad269495a01589527df6
Grzegorz Sztechmiler <grzegorz.sztechmiler@gmail.com>
2019-01-13 11:23:10 +0100

    staring rockets

--- SomeProgram.java
+++ SomeProgram.java
@@ -4,4 +4,7 @@ public static void main (String[] args){
 	System.out.println(We are learning");
 	System.out.println(We will be Git experts, soon.");
 	}
+	
+	System.out.println(Start rockets");
+	}
 }
